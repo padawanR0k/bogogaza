af9ce6038c4d01ca86b053404f1f2955a065e062
padawanr0k <padawanr0k@gmail.com>
2024-03-04 13:08:44 +0900

    fix: 구글 맵 컴포넌트 타입 추가

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,14 +1,70 @@
 "use client";
 
-import { Wrapper } from "@googlemaps/react-wrapper";
-import { ReactNode } from "react";
+import { Status, Wrapper } from "@googlemaps/react-wrapper";
+import { ReactElement } from "react";
+import { useEffect, useRef } from "react";
+
+// seoul
+const DEFAULT_CENTER = {
+  lat: 37.5665,
+  lng: 126.978,
+};
+const DEFAULT_ZOOM = 7;
 
 import mapStyles from "./Map.module.css";
 
-export const Map = ({ children }: { children?: ReactNode }) => (
+function Render(children?: ReactElement) {
+  return function RenderItem(status: string) {
+    switch (status) {
+      case Status.LOADING:
+        return <>...loading</>;
+      case Status.FAILURE:
+        return <>Error</>;
+      case Status.SUCCESS:
+        return children || <></>;
+      default:
+        return <>default</>;
+    }
+  };
+}
+
+const MapWrapper = ({ children }: { children?: ReactElement }) => (
   <div className={mapStyles.map}>
-    <Wrapper apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
+    <Wrapper
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+      render={Render(children)}
+    >
       {children}
     </Wrapper>
   </div>
 );
+
+function MapCore() {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    // Display the map
+    if (ref.current) {
+      new window.google.maps.Map(ref.current, {
+        center: DEFAULT_CENTER,
+        zoom: DEFAULT_ZOOM,
+      });
+    }
+  }, []);
+
+  return (
+    <div
+      id="map"
+      ref={ref}
+      style={{ width: "1000px", height: "700px", background: "white" }}
+    />
+  );
+}
+
+export const Map = () => {
+  return (
+    <MapWrapper>
+      <MapCore />
+    </MapWrapper>
+  );
+};
